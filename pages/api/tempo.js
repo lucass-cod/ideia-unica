1cfb889b5866e7be89441ae735ca3f918b720cec
Lucas <lucas0411.vasconcelos@gmail.com>
2024-08-22 15:33:41 -0300

    criação do arquivo tempo.js

---
+++ pages/api/tempo.js
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+
+function tempo(request, reposne) {
+  constDynmaicDate = new date();
+
+  Response.json({
+    date: dynamicDate.toGMTString()
+  })
+}
+
+export default tempo;
